a6cd97c2d07438c843f1b256d970dce1ec99d1c1
Abdullah Adel <abdullahadel.aam@gmail.com>
2020-03-07 12:35:09 +0200

    Setting up the project

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+//connecting to db
+mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("Connected to MongoDB")
+});
 
 //init app
 const app = express();
@@ -12,7 +22,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    res.render('index', {
+        title: 'Home'
+    });
 });
 
 //starting the server
