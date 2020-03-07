a452b122571c90890c966fa2f397cd55c7724f01
Abdullah Adel <abdullahadel.aam@gmail.com>
2020-03-07 14:12:26 +0200

    Nothing

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const config = require("./config/database");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator');
 
 //connecting to db
 mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,11 +24,49 @@ app.set('view engine', 'ejs');
 //public folder setup
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home'
-    });
+//body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//express-session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+//express-validator middleware
+app.use(expressValidator({
+    errorFormatter: (param, msg, value) => {
+        const namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+//express-messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
+
+//set routes
+const pages = require("./routes/pages");
+const adminPages = require("./routes/adminPages");
+
+app.use('/admin/pages', adminPages);
+app.use('/', pages);
 
 //starting the server
 const port = 7000;
